076c1639d55afb2617c38a7882b9a28c9e96c41c
salman <najarsalman4@gmail.com>
2022-08-25 09:30:06 +0530

    Php mailer Function Set for Corporate Hs /contact us

---
+++ SendEmail.php
@@ -0,0 +1,40 @@
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require('PHPMailer/Exception.php');
+
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'najarsalman4@gmail.com';                     //SMTP username
+    $mail->Password   = 'Salman@1231';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('najarsalman4@gmail.com', 'SalmanQuadir');
+    $mail->addAddress('najarsalman4@gmail.com');     //Add a recipient
+   
+
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Testing PHPMailer';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+ 
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
