600b23d3df2626adb8c2a0d5932e11ce719603f6
salman <najarsalman4@gmail.com>
2024-02-08 09:47:18 +0530

    footer changes/mail integrated

--- footer.php
+++ footer.php
@@ -2,8 +2,7 @@
    
     <div className="container ">
                 <div className="col-lg-12">
-                    <h1>Copyright © Healthcare Hub
-</h1>
+                    <h1>Copyright © Spinelle Healthcare</h1>
                 </div>
             </div>
 </div>
